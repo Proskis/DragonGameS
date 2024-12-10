62c23c8b01ff20c55bcb11a57c47166195f9a04a
Proskis <visscherwilliam@gmail.com>
2024-12-10 22:22:24 +0100

    Update Player.java

--- Player.java
+++ Player.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Player {
     
-     public static void PlayerName(){
+     public static void PlayerName(){ // metod för att sammla spelarens namn och skriva ut ett välkomst meddelande
          Scanner input = new Scanner(System.in);
         System.out.print("Please enter your name:  ");
             String player = input.next();
