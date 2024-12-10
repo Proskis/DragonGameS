5edc6a1ffc9bc7d62592a36b2a657149c17784da
Proskis <visscherwilliam@gmail.com>
2024-12-10 22:20:50 +0100

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,7 +1,14 @@
 
 package dragontreasure;
-
+import java.util.Scanner;
 
 public class Player {
     
+     public static void PlayerName(){
+         Scanner input = new Scanner(System.in);
+        System.out.print("Please enter your name:  ");
+            String player = input.next();
+            System.out.println();
+            System.out.println("Welcome " + player + "! To the DragonTreasure game");
+}
 }
