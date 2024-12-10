1dc93ae8f643f351df2f8bd518c8978050ff1c1f
Sauna03 <viktor.kruukka@hotmail.se>
2024-12-10 14:14:09 +0100

    Update Room.java

--- Room.java
+++ Room.java
@@ -1,7 +1,24 @@
 
 package dragontreasure;
 
-
 public class Room {
+    String name;
+    String description;
+    
+    public Room(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+    
+    //getter rum namnen 
+    public String getName() {
+        return name;
+    }
+    
+    //getter för beskrivning av rum
+    public String getDescription() {
+        return description;
+    }
     
 }
+
