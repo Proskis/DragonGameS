84f4f9207ad424c9fbfae76ab4854395b56d94db
Proskis <visscherwilliam@gmail.com>
2024-12-10 14:55:32 +0100

    Update Door.java

--- Door.java
+++ Door.java
@@ -4,4 +4,19 @@ package dragontreasure;
 
 public class Door {
     
+     char position;
+     boolean locked;
+     
+     
+    public Door(char position, boolean locked){
+        
+        this.position = position;
+       
+    }
+    public char getPosition() {
+        return position;
+    }
+    public boolean getLocked() {
+        return locked;
+    }
 }
