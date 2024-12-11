530b5a476e31eaf4993deaeb0edc22ef96b7e0c8
Proskis <visscherwilliam@gmail.com>
2024-12-11 11:55:13 +0100

    Update Player.java

--- Player.java
+++ Player.java
@@ -6,9 +6,17 @@ public class Player {
     
      public static void PlayerName(){ // metod för att samla spelarens namn och skriva ut ett välkomst meddelande
          Scanner input = new Scanner(System.in);
+         
+         //Promptar spelaren att ange sitt namn
         System.out.print("Please enter your name:  ");
             String player = input.next();
+           
             System.out.println();
             System.out.println("Welcome " + player + "! To the DragonTreasure game");
+            
+            
+            
 }
+     
+     
 }
