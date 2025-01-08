ad4126de681610c3b03ec87ed84200afa6d94cc1
Proskis <visscherwilliam@gmail.com>
2025-01-08 23:34:58 +0100

    Update Door.java

--- Door.java
+++ Door.java
@@ -1,17 +1,14 @@
-
 package dragontreasure;
 
-
 public class Door {
-    
-     String position;
-     boolean unLocked; 
+     private String position;
+     private boolean unLocked; 
      
      
      //metod för att hämta dörrens position (vart den leder) samt ifall dörren är upplåst
     public Door(String position, boolean unLocked){ 
-        
         this.position = position;
+        this.unLocked = unLocked;
        
     }
     //getter dörr position
@@ -20,7 +17,42 @@ public class Door {
     }
     //getter för ifall dörr är låst eller upplåst
     public boolean getLocked() {
-        unLocked = true;
         return unLocked;
+    }
+    // Statisk metod för att hämta en 2D-array av alla rummen
+    public static Door[][] getAllDoors() {
+        return new Door[][]{
+             { 
+                new Door("south", true),
+                
+                null,
+                
+                new Door("south", true)
+                
+                
+            },
+            
+            //rad 1 med kolumner
+            {
+                
+                new Door("north, east and south",true),
+                
+                null,
+
+                new Door("North", false)
+
+                
+            },
+            
+            //rad 2 med kolumner
+            {
+                new Door("north and east", true),
+                
+                new Door("west and east",true),
+                
+                new Door("north and west", true)
+           
+            }
+        };
     }
 }
